4a4588cfd7fa8e273fd18353b3aa374ad84c54c9
yichuan.deng <yichuan.deng@shopee.com>
2023-05-05 23:04:12 +0800

    add file

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -2,11 +2,20 @@
 <template>
   <div>
    <h1> {{message}} </h1>
+   <el-row>
+  <el-button>默认按钮</el-button>
+  <el-button type="primary">主要按钮</el-button>
+  <el-button type="success">成功按钮</el-button>
+  <el-button type="info">信息按钮</el-button>
+  <el-button type="warning">警告按钮</el-button>
+  <el-button type="danger">危险按钮</el-button>
+</el-row>
   </div>
+
 </template>
 
 <script >
-export default {
+const Index = {
   name: 'Index',
   data () {
     return {
@@ -14,4 +23,5 @@ export default {
     }
   }
 }
+export default Index
 </script>
---
+++ src/views/User.vue
@@ -0,0 +1,20 @@
+
+<template>
+  <div>
+   <h1> {{message1}} </h1>
+  </div>
+
+</template>
+
+<script >
+
+const User = {
+  name: 'User',
+  data () {
+    return {
+      'message1': 'Hello User'
+    }
+  }
+}
+export default User
+</script>
